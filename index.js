816e606b0231f567033527d555660ad7143614e2
Abdullah <alihamza@wearespark.me>
2026-09-15 20:04:20 +0500

    toggale system

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()"; 
+
 let passGen = document.getElementById("passGen")
 let PasswordEl = document.getElementById("password")
 let PasswordEltwo = document.getElementById("password-two")
 let userInput = document.getElementById("userInput")
 let warrning = document.getElementById("warrning")
+let toggleEl = document.getElementById("toggle")
+
 let showWarrning = false
 let pass = []
 let passtwo = []
+
+function ToggleHander(){
+    let ischecked = event.target.checked
+
+    if (ischecked){
+        console.log("true")
+    }else if(!ischecked){
+        console.log("false")
+    }
+}
+
+
 
 function warningShower() {
     if (showWarrning === true) {
@@ -65,6 +84,11 @@ function passwordGen() {
     PasswordEltwo.textContent = passtwo.join("")
 }
 
+
+// event Listeners
 passGen.addEventListener("click", function () {
     passwordGen()
+})
+toggleEl.addEventListener("change", function(){
+    ToggleHander()
 })
